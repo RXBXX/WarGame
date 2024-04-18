fc8c563e635894b17dba3fede933722f5d616ebd
xiakunpeng <1293647857@qq.com>
2024-04-18 08:33:42 +0800

    Revert "#00001 #工程文件上传"
    This reverts commit 0358d3d8ce0844e264a1a242b1320a94e007fa98.

--- Assets/Editor/BuildMap.cs
+++ Assets/Editor/BuildMap.cs
@@ -4,220 +4,152 @@ using UnityEditor.SceneManagement;
 using System.IO;
 using System.Collections.Generic;
 
-namespace WarGame
+public class BuildMap : EditorWindow
 {
-    public class BuildMap : EditorWindow
+    private const string ToolTitle = "地图编辑器";
+    private const string MapPath = "Scenes/MapEditorScene";
+    /// <summary>
+    /// 打开地图编辑器
+    /// </summary>
+    [MenuItem("Tools/" + ToolTitle)]
+    public static void OpenMapEditor()
     {
-        private const string ToolTitle = "地图编辑器";
-        private const string MapPath = "Assets/Scenes/MapEditorScene.unity";
-        private static Dictionary<Enum.HexagonType, int> weights = new Dictionary<Enum.HexagonType, int>();
-        private static int xNum, yNum, zNum;
+        var wd = GetWindow<BuildMap>();
+        wd.titleContent = new GUIContent(ToolTitle);
+    }
 
-        /// <summary>
-        /// 打开地图编辑器
-        /// </summary>
-        [MenuItem("Tools/" + ToolTitle)]
-        public static void OpenMapEditor()
+    public void OnGUI()
+    {
+        EditorGUILayout.Space();
+
+        //打开地图编辑场景
+        if (GUILayout.Button("打开地图编辑场景"))
         {
-            var wd = GetWindow<BuildMap>();
-            wd.titleContent = new GUIContent(ToolTitle);
-
-            foreach (var v in System.Enum.GetValues(typeof(Enum.HexagonType)))
-            {
-                weights[(Enum.HexagonType)v] = 1;
-            }
+            OpenMapScene();
         }
 
-        public void OnGUI()
+        EditorGUILayout.Space();
+
+        //清空地图编辑场景
+        if (GUILayout.Button("清空地图编辑场景"))
         {
-            EditorGUILayout.Space();
-
-            //打开地图编辑场景
-            if (GUILayout.Button("打开地图编辑场景"))
-            {
-                OpenMapScene();
-            }
-
-            EditorGUILayout.Space();
-
-            //清空地图编辑场景
-            if (GUILayout.Button("清空地图编辑场景"))
-            {
-                ClearMapScene();
-            }
-
-            EditorGUILayout.Space();
-
-            //导出地图
-            if (GUILayout.Button("导出地图"))
-            {
-                SaveMap();
-            }
-
-            EditorGUILayout.Space();
-
-            // 打开地图
-            GUILayout.Label("当前选中地图：");
-            if (GUILayout.Button("打开地图"))
-            {
-                OpenMap();
-            }
-
-            EditorGUILayout.Space();
-
-            // 快速生成地图
-            GUILayout.Label("快速生成地图：");
-            GUILayout.Label("X数量：");
-            int.TryParse(GUILayout.TextField(xNum.ToString()), out xNum);
-            GUILayout.Label("Y数量：");
-            int.TryParse(GUILayout.TextField(yNum.ToString()), out yNum);
-            GUILayout.Label("Z数量：");
-            int.TryParse(GUILayout.TextField(zNum.ToString()), out zNum);
-
-            foreach (var v in System.Enum.GetValues(typeof(Enum.HexagonType)))
-            {
-                var type = (Enum.HexagonType)v;
-                GUILayout.Label(type + " 权重：");
-                int a = 0;
-                int.TryParse(GUILayout.TextField(weights[type].ToString()), out a);
-                weights[type] = a;
-            }
-            if (GUILayout.Button("打开地图"))
-            {
-                QuickGenerageMap(xNum, yNum, zNum, weights);
-            }
-        }
-
-        /// <summary>
-        /// 打开地图编辑专用的场景
-        /// </summary>
-        private void OpenMapScene()
-        {
-            if (Application.isPlaying)
-                return;
-
-            EditorSceneManager.OpenScene(MapPath);
-        }
-
-        /// <summary>
-        /// 导出地图的接口
-        /// </summary>
-        private void SaveMap()
-        {
-            if (!MapTool.Instance.IsActiveMapEditor())
-                return;
-
-            var dir = EditorUtility.SaveFilePanel("导出地图", Application.dataPath + "/Maps", "地图", "json");
-
-            var rootObj = GameObject.Find("Root");
-            var hexagonCount = rootObj.transform.childCount;
-            HexagonCell[] hexagons = new HexagonCell[hexagonCount];
-
-            for (int i = 0; i < hexagonCount; i++)
-            {
-                var hexagonTra = rootObj.transform.GetChild(i);
-                var data = hexagonTra.GetComponent<HexagonCellData>();
-                var hexagonCell = new HexagonCell(i, data.config);
-                hexagonCell.position = MapTool.Instance.FromWorldPosToCellPos(hexagonTra.position);
-                hexagons[i] = hexagonCell;
-            }
-
-            var jsonStr = WarGame.Tool.Instance.ToJson(hexagons);
-            try { File.WriteAllText(dir, jsonStr); }
-            catch (IOException exception)
-            {
-                Debug.Log(exception);
-            };
-        }
-
-        /// <summary>
-        /// 打开地图的接口
-        /// </summary>
-        private void OpenMap()
-        {
-            if (!MapTool.Instance.IsActiveMapEditor())
-                return;
-
             ClearMapScene();
-
-            var dir = EditorUtility.OpenFilePanel("打开地图", Application.dataPath + "/Maps", "");
-            var jsonStr = File.ReadAllText(dir);
-
-            var rootObj = GameObject.Find("Root");
-            HexagonCell[] hexagons = WarGame.Tool.Instance.FromJson<HexagonCell[]>(jsonStr);
-
-            for (int i = 0; i < hexagons.Length; i++)
-            {
-                var hexagon = hexagons[i];
-                string assetPath = MapTool.Instance.GetHexagonPrefab(hexagon.config.type);
-                GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>(assetPath);
-                var obj = Instantiate(prefab);
-                obj.transform.position = MapTool.Instance.FromCellPosToWorldPos(hexagon.position);
-                obj.transform.SetParent(rootObj.transform);
-            }
         }
 
-        //快速生成地图
-        private void QuickGenerageMap(int xNum, int yNum, int zNum, Dictionary<Enum.HexagonType, int> weightList)
+        EditorGUILayout.Space();
+
+        //导出地图
+        if (GUILayout.Button("导出地图"))
         {
-            if (!MapTool.Instance.IsActiveMapEditor())
-                return;
+            SaveMap();
+        }
 
-            ClearMapScene();
+        EditorGUILayout.Space();
 
-            var rootObj = GameObject.Find("Root");
+        // 打开地图
+        GUILayout.Label("当前选中地图：");
+        if (GUILayout.Button("打开地图"))
+        {
+            OpenMap();
+        }
+    }
 
-            Dictionary<int, Enum.HexagonType> castDic = new Dictionary<int, Enum.HexagonType>();
-            int weightBase = 0;
-            foreach (var pair in weightList)
+    /// <summary>
+    /// 打开地图编辑专用的场景
+    /// </summary>
+    private void OpenMapScene()
+    {
+        EditorSceneManager.OpenScene(MapPath);
+    }
+
+    /// <summary>
+    /// 导出地图的接口
+    /// </summary>
+    private void SaveMap()
+    {
+        var dir = EditorUtility.SaveFilePanel("导出地图", Application.dataPath + "/Maps", "地图", "json");
+
+        var rootObj = GameObject.Find("Root");
+        var hexagonCount = rootObj.transform.childCount;
+        HexagonCell[] hexagons = new HexagonCell[hexagonCount];
+
+        for (int i = 0; i < hexagonCount; i++)
+        {
+            var hexagonTra = rootObj.transform.GetChild(i);
+            var data = hexagonTra.GetComponent<HexagonCellData>();
+            Debug.Log(data.config.type);
+            var hexagonCell = new HexagonCell(i, data.config);
+            hexagonCell.position = MapTool.Instance.FromWorldPosToCellPos(hexagonTra.position);
+            hexagons[i] = hexagonCell;
+        }
+
+        var jsonStr = WarGame.Tool.Instance.ToJson(hexagons);
+        try { File.WriteAllText(dir, jsonStr); }
+        catch (IOException exception)
+        {
+            Debug.Log(exception);
+        };
+    }
+
+    /// <summary>
+    /// 打开地图的接口
+    /// </summary>
+    private void OpenMap()
+    {
+        //Undo.RecordObject(transform, "Open Map");
+        ClearMapScene();
+
+        var dir = EditorUtility.OpenFilePanel("打开地图", Application.dataPath + "/Maps", "");
+        var jsonStr = File.ReadAllText(dir);
+
+        var rootObj = GameObject.Find("Root");
+        HexagonCell[] hexagons = WarGame.Tool.Instance.FromJson<HexagonCell[]>(jsonStr);
+        Dictionary<Enum.HexagonType, GameObject> prefabDic = new Dictionary<Enum.HexagonType, GameObject>();
+
+        for (int i = 0; i < hexagons.Length; i++)
+        {
+            var hexagon = hexagons[i];
+            string assetPath = "";
+            GameObject prefab = null;
+            if (!prefabDic.TryGetValue(hexagon.config.type, out prefab))
             {
-                weightBase += pair.Value;
-                castDic[weightBase] = pair.Key;
-            }
-
-            for (int i = 0; i < xNum; i++)
-            {
-                for (int j = 0; j < zNum; j++)
+                switch (hexagon.config.type)
                 {
-                    for (int q = 0; q < yNum; q++)
-                    {
-                        var rd = Random.Range(0, weightBase);
-                        var minKey = weightBase;
-                        Enum.HexagonType type = Enum.HexagonType.BeachShore;
-                        foreach (var pair in castDic)
-                        {
-                            if (rd < pair.Key && pair.Key < minKey)
-                            {
-                                minKey = pair.Key;
-                                type = pair.Value;
-                            }
-                        }
-
-                        string assetPath = MapTool.Instance.GetHexagonPrefab(type);
-                        GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>(assetPath);
-                        var obj = Instantiate(prefab);
-                        obj.transform.position = MapTool.Instance.FromCellPosToWorldPos(new Vector3(i, q, j));
-                        obj.transform.SetParent(rootObj.transform);
-                    }
+                    case Enum.HexagonType.BeachShore:
+                        assetPath = "Assets/Low Poly Hexagons/Assets/Prefabs/Hexagons/BeachShore.prefab";
+                        break;
+                    case Enum.HexagonType.DigSite:
+                        assetPath = "Assets/Low Poly Hexagons/Assets/Prefabs/Hexagons/DigSite.prefab";
+                        break;
+                    case Enum.HexagonType.HellLake:
+                        assetPath = "Assets/Low Poly Hexagons/Assets/Prefabs/Hexagons/HellLake.prefab";
+                        break;
+                    case Enum.HexagonType.Lake:
+                        assetPath = "Assets/Low Poly Hexagons/Assets/Prefabs/Hexagons/Lake.prefab";
+                        break;
                 }
+                Debug.Log(assetPath);
+
+                prefab = AssetDatabase.LoadAssetAtPath<GameObject>(assetPath);
+                prefabDic[hexagon.config.type] = prefab;
             }
+            var obj = Instantiate(prefab);
+            obj.transform.position = MapTool.Instance.FromCellPosToWorldPos(hexagon.position);
+            obj.transform.SetParent(rootObj.transform);
         }
+    }
 
-        /// <summary>
-        /// 清空场景
-        /// </summary>
-        private void ClearMapScene()
+    /// <summary>
+    /// 清空场景
+    /// </summary>
+    private void ClearMapScene()
+    {
+        var rootObj = GameObject.Find("Root");
+        var hexagonCount = rootObj.transform.childCount;
+
+        for (int i = hexagonCount - 1; i >= 0; i--)
         {
-            if (!MapTool.Instance.IsActiveMapEditor())
-                return;
-
-            var rootObj = GameObject.Find("Root");
-            var hexagonCount = rootObj.transform.childCount;
-
-            for (int i = hexagonCount - 1; i >= 0; i--)
-            {
-                DestroyImmediate(rootObj.transform.GetChild(i).gameObject);
-            }
+            DestroyImmediate(rootObj.transform.GetChild(i).gameObject);
         }
     }
 }
--- Assets/Editor/CustomTransformEditor.cs
+++ Assets/Editor/CustomTransformEditor.cs
@@ -1,219 +1,210 @@
 using UnityEngine;
 using UnityEditor;
-using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
 
-namespace WarGame
+[CustomEditor(typeof(Transform))]
+public class CustomTransformEditor : Editor
 {
-    [CustomEditor(typeof(Transform))]
-    public class CustomTransformEditor : Editor
+    private bool customAxesEnabled = true;
+
+    //自定义轴向
+    private Vector3[] customAxes = new Vector3[3]; // 修改为你想要的轴数量
+
+    //当前选中的拖拽轴
+    private int dragingAxis = -1;
+
+    private Vector2 mousePos = Vector2.zero;
+
+    private Vector3 tempPos = Vector2.zero;
+
+    ////六边形内径
+    //private float InsideDiameter = 1.0f;
+
+    ////z轴偏移弧度 
+    //private const float Radian = 30.0F / 180.0F * Mathf.PI;
+
+
+    ///在C#中，构造函数（也称为实例构造函数）是用于初始化类的实例的特殊方法。构造函数不允许声明为静态的（即static），因为静态成员是属于类本身而不是类的实例，
+    ///因此构造函数不能是静态的。
+    ///如果你希望执行一些初始化逻辑，并且希望该逻辑在类第一次被使用时执行，你可以考虑使用静态构造函数（static constructor）。
+    ///静态构造函数是用于初始化类的静态成员的，它在类第一次被使用时自动调用，并且只会被调用一次。静态构造函数不能带有访问修饰符，也不能接受任何参数。
+    static CustomTransformEditor()
     {
-        private bool customAxesEnabled = true;
+        // 注册场景变化事件
+        EditorApplication.hierarchyChanged += OnHierarchyChanged;
+    }
 
-        //自定义轴向
-        private Vector3[] customAxes = new Vector3[3]; // 修改为你想要的轴数量
+    private void OnEnable()
+    {
+        customAxes[0] = new Vector3(1, 0, 0);
+        customAxes[1] = new Vector3(Mathf.Sin(30.0f / 180.0f * Mathf.PI), 0, Mathf.Cos(30.0f / 180.0f * Mathf.PI));
+        customAxes[2] = Vector3.up;
+    }
 
-        //当前选中的拖拽轴
-        private int dragingAxis = -1;
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
 
-        private Vector2 mousePos = Vector2.zero;
+        Transform transform = (Transform)target;
 
-        private Vector3 tempPos = Vector2.zero;
+        EditorGUILayout.Space();
+        EditorGUILayout.LabelField("Custom Axes", EditorStyles.boldLabel);
+        customAxesEnabled = EditorGUILayout.Toggle("Enable Custom Axes", customAxesEnabled);
 
-
-        ///在C#中，构造函数（也称为实例构造函数）是用于初始化类的实例的特殊方法。构造函数不允许声明为静态的（即static），因为静态成员是属于类本身而不是类的实例，
-        ///因此构造函数不能是静态的。
-        ///如果你希望执行一些初始化逻辑，并且希望该逻辑在类第一次被使用时执行，你可以考虑使用静态构造函数（static constructor）。
-        ///静态构造函数是用于初始化类的静态成员的，它在类第一次被使用时自动调用，并且只会被调用一次。静态构造函数不能带有访问修饰符，也不能接受任何参数。
-        static CustomTransformEditor()
+        if (customAxesEnabled)
         {
-            // 注册场景变化事件
-            EditorApplication.hierarchyChanged += OnHierarchyChanged;
-        }
+            EditorGUI.indentLevel++;
 
-        private void OnEnable()
-        {
-            if (!MapTool.Instance.IsActiveMapEditor())
-                return;
-
-            customAxes[0] = new Vector3(1, 0, 0);
-            customAxes[1] = new Vector3(Mathf.Sin(30.0f / 180.0f * Mathf.PI), 0, Mathf.Cos(30.0f / 180.0f * Mathf.PI));
-            customAxes[2] = Vector3.up;
-        }
-
-        public override void OnInspectorGUI()
-        {
-            base.OnInspectorGUI();
-
-            if (!MapTool.Instance.IsActiveMapEditor())
-                return;
-
-            Transform transform = (Transform)target;
-
-            EditorGUILayout.Space();
-            EditorGUILayout.LabelField("Custom Axes", EditorStyles.boldLabel);
-            customAxesEnabled = EditorGUILayout.Toggle("Enable Custom Axes", customAxesEnabled);
-
-            if (customAxesEnabled)
-            {
-                EditorGUI.indentLevel++;
-
-                for (int i = 0; i < customAxes.Length; i++)
-                {
-                    EditorGUI.BeginChangeCheck();
-                    Vector3 customAxis = EditorGUILayout.Vector3Field("Axis " + i, customAxes[i]);
-                    if (EditorGUI.EndChangeCheck())
-                    {
-                        Undo.RecordObject(transform, "Change Custom Axes");
-                        customAxes[i] = customAxis;
-                    }
-                }
-
-                EditorGUI.indentLevel--;
-            }
-
-            // 隐藏编辑视图下默认的操作轴
-            // 获取当前Scene视图中的工具
-            UnityEditor.Tool currentTool = UnityEditor.Tools.current;
-            // 重置工具
-            if ((currentTool == UnityEditor.Tool.Move || currentTool == UnityEditor.Tool.Rect))
-            {
-                Tools.current = UnityEditor.Tool.None;
-            }
-            if (currentTool == UnityEditor.Tool.Rotate)
-            {
-                Tools.current = UnityEditor.Tool.None;
-            }
-            if (currentTool == UnityEditor.Tool.Scale)
-            {
-                Tools.current = UnityEditor.Tool.None;
-            }
-        }
-
-        private void OnSceneGUI()
-        {
-            if (!MapTool.Instance.IsActiveMapEditor())
-                return;
-
-            if (!customAxesEnabled)
-                return;
-
-            Transform transform = (Transform)target;
             for (int i = 0; i < customAxes.Length; i++)
             {
-                Handles.color = GetAxisColor(i);
-                Vector3 worldAxis = transform.TransformDirection(customAxes[i]);
-                Vector3 moveVector = worldAxis.normalized;
-
-                // 在场景视图中绘制自定义位移轴
-                Handles.ArrowHandleCap(
-                    GUIUtility.GetControlID(FocusType.Passive),
-                    transform.position,
-                    Quaternion.LookRotation(worldAxis),
-                    HandleUtility.GetHandleSize(Vector3.zero) + 0.1F,
-                    EventType.Repaint
-                );
-
-                // 获取位移轴的起点和终点
-                Vector3 startPosition = transform.position;
-                Vector3 endPosition = transform.position + moveVector * HandleUtility.GetHandleSize(Vector3.zero);
-
-                // 检测鼠标事件
-                Event guiEvent = Event.current;
-                //Vector3 mousePos = HandleUtility.GUIPointToWorldRay(guiEvent.mousePosition).origin;
-                switch (guiEvent.type)
+                EditorGUI.BeginChangeCheck();
+                Vector3 customAxis = EditorGUILayout.Vector3Field("Axis " + i, customAxes[i]);
+                if (EditorGUI.EndChangeCheck())
                 {
-                    case EventType.MouseDown:
-                        if (guiEvent.button == 0 && HandleUtility.DistanceToLine(endPosition, startPosition) < 10f)
-                        {
-                            dragingAxis = i;
-                            mousePos = guiEvent.mousePosition;
-                            tempPos = transform.position;
-                            GUIUtility.hotControl = GUIUtility.GetControlID(FocusType.Passive);
-                            guiEvent.Use();
-                        }
-                        break;
-                    case EventType.MouseDrag:
-                        if (dragingAxis >= 0)
-                        {
-                            Undo.RecordObject(transform, "Move Object");
+                    Undo.RecordObject(transform, "Change Custom Axes");
+                    customAxes[i] = customAxis;
+                }
+            }
 
-                            var oldPos = transform.position;
-                            //将鼠标拖动映射到沿 3D 空间中一条直线的运动。
-                            Vector3 tempWorldAxis = transform.TransformDirection(customAxes[dragingAxis]);
-                            Vector3 tempMoveVector = tempWorldAxis.normalized;
-                            float delta = HandleUtility.CalcLineTranslation(mousePos, guiEvent.mousePosition, oldPos, tempMoveVector);
-                            Vector3 newPos = tempMoveVector * delta + tempPos;
+            EditorGUI.indentLevel--;
+        }
 
-                            var hexMapPos = MapTool.Instance.FromWorldPosToCellPos(newPos);
-                            newPos = MapTool.Instance.FromCellPosToWorldPos(hexMapPos);
+        // 隐藏编辑视图下默认的操作轴
+        // 获取当前Scene视图中的工具
+        Tool currentTool = Tools.current;
+        // 重置工具
+        if ((currentTool == Tool.Move || currentTool == Tool.Rect))
+        {
+            Tools.current = Tool.None;
+        }
+        if (currentTool == Tool.Rotate)
+        {
+            Tools.current = Tool.None;
+        }
+        if (currentTool == Tool.Scale)
+        {
+            Tools.current = Tool.None;
+        }
+    }
 
-                            var posDirty = false;
-                            if (0 == dragingAxis && newPos.x != oldPos.x)
-                            {
-                                posDirty = true;
-                            }
-                            else if (1 == dragingAxis && newPos.x != oldPos.x && newPos.z != oldPos.z)
-                            {
-                                posDirty = true;
-                            }
-                            else if (2 == dragingAxis && newPos.y != oldPos.y)
-                            {
-                                posDirty = true;
-                            }
+    private void OnSceneGUI()
+    {
+        if (!customAxesEnabled)
+            return;
 
-                            if (posDirty)
-                            {
-                                transform.position = newPos;
-                                //mousePos = guiEvent.mousePosition;
-                                //tempPos = newPos;
-                            }
+        Transform transform = (Transform)target;
+        for (int i = 0; i < customAxes.Length; i++)
+        {
+            Handles.color = GetAxisColor(i);
+            Vector3 worldAxis = transform.TransformDirection(customAxes[i]);
+            Vector3 moveVector = worldAxis.normalized;
 
-                            HandleUtility.Repaint();
-                            guiEvent.Use();
-                        }
-                        break;
-                    case EventType.MouseUp:
-                        dragingAxis = -1;
-                        mousePos = Vector2.zero;
-                        GUIUtility.hotControl = 0;
+            // 在场景视图中绘制自定义位移轴
+            Handles.ArrowHandleCap(
+                GUIUtility.GetControlID(FocusType.Passive),
+                transform.position,
+                Quaternion.LookRotation(worldAxis),
+                HandleUtility.GetHandleSize(Vector3.zero) + 0.1F,
+                EventType.Repaint
+            );
+
+            // 获取位移轴的起点和终点
+            Vector3 startPosition = transform.position;
+            Vector3 endPosition = transform.position + moveVector * HandleUtility.GetHandleSize(Vector3.zero);
+
+            // 检测鼠标事件
+            Event guiEvent = Event.current;
+            //Vector3 mousePos = HandleUtility.GUIPointToWorldRay(guiEvent.mousePosition).origin;
+            switch (guiEvent.type)
+            {
+                case EventType.MouseDown:
+                    if (guiEvent.button == 0 && HandleUtility.DistanceToLine(endPosition, startPosition) < 10f)
+                    {
+                        dragingAxis = i;
+                        mousePos = guiEvent.mousePosition;
+                        tempPos = transform.position;
+                        GUIUtility.hotControl = GUIUtility.GetControlID(FocusType.Passive);
                         guiEvent.Use();
-                        break;
-                }
-            }
-        }
+                    }
+                    break;
+                case EventType.MouseDrag:
+                    if (dragingAxis >= 0)
+                    {
+                        Undo.RecordObject(transform, "Move Object");
 
-        private Color GetAxisColor(int index)
-        {
-            switch (index)
-            {
-                case 0:
-                    return Color.red;
-                case 1:
-                    return Color.green;
-                case 2:
-                    return Color.blue;
-                default:
-                    return Color.white;
-            }
-        }
+                        var oldPos = transform.position;
+                        //将鼠标拖动映射到沿 3D 空间中一条直线的运动。
+                        Vector3 tempWorldAxis = transform.TransformDirection(customAxes[dragingAxis]);
+                        Vector3 tempMoveVector = tempWorldAxis.normalized;
+                        float delta = HandleUtility.CalcLineTranslation(mousePos, guiEvent.mousePosition, oldPos, tempMoveVector);
+                        Vector3 newPos = tempMoveVector * delta + tempPos;
 
-        //当对象被新增到场景中，添加到Root节点下
-        private static void OnHierarchyChanged()
-        {
-            if (!MapTool.Instance.IsActiveMapEditor())
-                return;
+                        var hexMapPos = MapTool.Instance.FromWorldPosToCellPos(newPos);
+                        newPos = MapTool.Instance.FromCellPosToWorldPos(hexMapPos);
 
-            var rootObj = GameObject.Find("Root");
+                        var posDirty = false;
+                        if (0 == dragingAxis && newPos.x != oldPos.x)
+                        {
+                            posDirty = true;
+                        }
+                        else if (1 == dragingAxis && newPos.x != oldPos.x && newPos.z != oldPos.z)
+                        {
+                            posDirty = true;
+                        }
+                        else if (2 == dragingAxis && newPos.y != oldPos.y)
+                        {
+                            posDirty = true;
+                        }
 
-            // 获取场景中的所有地块
-            GameObject[] rootObjects = GameObject.FindGameObjectsWithTag(Enum.Tag.Hexagon.ToString());
-            for (int i = 0; i < rootObjects.Length; i++)
-            {
-                if (rootObjects[i].transform.parent != rootObj)
-                {
-                    rootObjects[i].transform.SetParent(rootObj.transform);
-                }
+                        if (posDirty)
+                        {
+                            transform.position = newPos;
+                            //mousePos = guiEvent.mousePosition;
+                            //tempPos = newPos;
+                        }
+
+                        HandleUtility.Repaint();
+                        guiEvent.Use();
+                    }
+                    break;
+                case EventType.MouseUp:
+                    dragingAxis = -1;
+                    mousePos = Vector2.zero;
+                    GUIUtility.hotControl = 0;
+                    guiEvent.Use();
+                    break;
             }
         }
     }
-}
+
+    private Color GetAxisColor(int index)
+    {
+        switch (index)
+        {
+            case 0:
+                return Color.red;
+            case 1:
+                return Color.green;
+            case 2:
+                return Color.blue;
+            default:
+                return Color.white;
+        }
+    }
+
+    //当对象被新增到场景中，添加到Root节点下
+    private static void OnHierarchyChanged()
+    {
+        var rootObj = GameObject.Find("Root");
+
+        // 获取场景中的所有地块
+        GameObject[] rootObjects = GameObject.FindGameObjectsWithTag(Enum.Tag.Hexagon.ToString());
+        for (int i = 0; i < rootObjects.Length; i++)
+        {
+            if (rootObjects[i].transform.parent != rootObj)
+            {
+                rootObjects[i].transform.SetParent(rootObj.transform);
+            }
+        }
+    }
+} 
